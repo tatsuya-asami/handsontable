95616f16d7e01736e3489e74e92b0bdf1946f7de
astatsuya <sharekobekko@gmail.com>
2019-04-30 15:19:24 +0900

    successed initial rendering

---
+++ src/components/Table.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <HotTable :settings="settings"/>
+  </div>
+</template>
+
+<script>
+import { HotTable } from "@handsontable/vue";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    HotTable
+  },
+  data: function() {
+    return {
+      settings: {
+        data: [
+          ["", "Ford", "Volvo", "Toyota", "Honda"],
+          ["2016", 10, 11, 12, 13],
+          ["2017", 20, 11, 14, 13],
+          ["2018", 30, 15, 12, 13]
+        ],
+        colHeaders: true,
+        rowHeaders: true
+      }
+    };
+  }
+};
+</script>
+
+<style src="../../node_modules/handsontable/dist/handsontable.full.css"></style>
+
