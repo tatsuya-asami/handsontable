82884dd86b3a670f5e23c5a458aeca9cb6b4fc2a
astatsuya <sharekobekko@gmail.com>
2019-05-26 22:01:52 +0900

    add google auth

--- src/components/authSettings.js
+++ src/components/authSettings.js
@@ -26,9 +26,9 @@ const uiConfig = {
   signInSuccessUrl: "/table",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
     // firebase.auth.PhoneAuthProvider.PROVIDER_ID
